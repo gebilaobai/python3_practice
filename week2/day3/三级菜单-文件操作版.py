e50a6072b1439a879c94d3e58a7dbf3b0a1a95dc
baiy <baiy@glodon.com>
2018-10-22 17:25:30 +0800

    文件操作练习+作业说明

---
+++ week2/day3/三级菜单-文件操作版.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Time    : 2018/10/20 14:02
+# Author  : NeoBY
+# Email   : baiyangbbyy@163.com
+# File    : lesson_file.py
+# Software: PyCharm
+
+"""
+三级菜单—文件操作版
+1、展示省市县（数据存在文件中）
+2、对菜单进行增删改操作
+涉及到的方法：
+a = str({'北京':{'海淀':'上地'}}) # 将字典转为字符串
+b = eval(a)  # 将字符串转换回字典
+"""
