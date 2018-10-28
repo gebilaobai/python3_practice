6bdae3e51ea7e96eaaa8aa5f8ae8f34f4000599f
NeoBY <baiyangbbyy@163.com>
2018-10-28 23:04:42 +0800

    提交生成器练习内容

--- week4/day4/生成器.py
+++ week4/day4/生成器.py
@@ -4,3 +4,13 @@
 # Email   : baiyangbbyy@163.com
 # File    : 生成器.py
 # Software: PyCharm
+
+s = (x * 2 for x in range(10))
+print(s)  # s为返回的生成器对象<generator object <genexpr> at 0x0000000002431ED0>
+
+print(s.__next__())  # 不推荐这种写法
+
+print(next(s))  # 推荐这种写法 等价于s.__next__() 在python2中用这种写法：s.next()
+
+
+
