be004791eed0bd7b34f9a4268d5af9a3408d21f9
NeoBY <baiyangbbyy@163.com>
2018-10-28 22:38:39 +0800

    提交列表生成式练习内容

---
+++ week4/day4/列表生成式.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Time    : 2018/10/28 22:25
+# Author  : NeoBY
+# Email   : baiyangbbyy@163.com
+# File    : 列表生成式.py
+# Software: PyCharm
+
+# 列表生成式
+# a = [x for x in range(10)]
+# print(a)
+#
+# a = [x*2 for x in range(10)]
+# print(a)
+#
+# a = [x*x for x in range(10)]
+# print(a)
+
+# def f(n):
+#     return n ** 3
+#
+#
+# a = [f(x) for x in range(10)]
+# print(a)
+
+
+t = ('123', 8)
+a, b = t  # 等同于 a = t[0],b=t[1]
+
+print(a)
+print(b)
---
+++ week4/day4/生成器.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Time    : 2018/10/28 22:38
+# Author  : NeoBY
+# Email   : baiyangbbyy@163.com
+# File    : 生成器.py
+# Software: PyCharm
