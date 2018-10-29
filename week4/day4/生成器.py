f978f4c1e72fef408184787eb4532ed0ea001ebe
baiy <baiy@glodon.com>
2018-10-29 21:42:40 +0800

    提交生成器相关练习

--- week4/day4/生成器.py
+++ week4/day4/生成器.py
@@ -4,13 +4,27 @@
 # Email   : baiyangbbyy@163.com
 # File    : 生成器.py
 # Software: PyCharm
+"""
+生成器就是一个可迭代对象iterable
+"""
 
-s = (x * 2 for x in range(10))
+
+
+s = (x * 2 for x in range(5))
 print(s)  # s为返回的生成器对象<generator object <genexpr> at 0x0000000002431ED0>
 
-print(s.__next__())  # 不推荐这种写法
+# print(s.__next__())  # 不推荐这种写法
+#
+# print(next(s))  # 推荐这种写法 等价于s.__next__() 在python2中用这种写法：s.next()
+# print(next(s))
+# print(next(s))
+# print(next(s))
+# print(next(s))  # 超过生成器能生成的数据范围则提示StopIteration
 
-print(next(s))  # 推荐这种写法 等价于s.__next__() 在python2中用这种写法：s.next()
+
+# 通过for循环从生成器中拿东西
+for i in s:
+    print(i)
 
 
 
