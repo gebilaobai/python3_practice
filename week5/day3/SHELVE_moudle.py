caf8cafed183e18788b8dd5cca0dfa75488733a6
baiy <baiy@glodon.com>
2018-11-12 14:03:49 +0800

    提交shelve练习内容

--- week5/day3/SHELVE_moudle.py
+++ week5/day3/SHELVE_moudle.py
@@ -5,6 +5,10 @@
 # File    : SHELVE_moudle.py
 # Software: PyCharm
 
+"""
+shelve模块比pickle模块简单，只有一个open函数，返回类似字典的对象，可读可写：key必须为字符串，而值可以是python锁支持的数据类型
+"""
+
 
 import shelve
 
@@ -20,3 +24,15 @@ data = f.get('info')
 print(data)
 data = f.get('shopping')
 print(data)
+
+
+
+# 补充内容，字典的.get方法
+# d = {'name':'alex','age':'18'}
+#
+# print(d['name'])
+#
+# print(d.get('name'))
+#
+# # 如果不加第二个参数就返回原本的，如果有第二个参数就返回第二个参数的值
+# print(d.get('sex','male'))
