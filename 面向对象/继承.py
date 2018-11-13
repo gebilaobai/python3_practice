26de382a664463e398b12436301dc8703ab800be
NeoBY <baiyangbbyy@163.com>
2018-11-13 23:38:46 +0800

    提交练习内容:继承

---
+++ 面向对象/继承.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Time    : 2018/11/13 23:21
+# Author  : NeoBY
+# Email   : baiyangbbyy@163.com
+# File    : 继承.py
+# Software: PyCharm
+
+# 继承
+class GrandFather:
+    def drink_beer(self):
+        pass
+
+
+class Father(GrandFather):  # 父类、基类
+    def basktball(self):
+        pass
+
+    def football(self):
+        pass
+
+    def somke(self):
+        print("I like smoke")
+
+    def drink(self):
+        print("I like drink")
+
+
+class Son(Father):  # 子类、派生类
+    def driver(self):
+        pass
+
+    def somke(self):  # 重写
+        print("I don't like smoke")
+
+
+s = Son()
+s.somke()
+s.drink()
