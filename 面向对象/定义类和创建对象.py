09a06bb6b6b4c92e6258108fe7845bae077c3742
NeoBY <baiyangbbyy@163.com>
2018-11-13 23:19:53 +0800

    提交练习内容:定义类和创建对象

--- 面向对象/定义类和创建对象.py
+++ 面向对象/定义类和创建对象.py
@@ -2,7 +2,7 @@
 # Time    : 2018/11/13 10:36
 # Author  : NeoBY
 # Email   : baiyangbbyy@163.com
-# File    : temp1.py
+# File    : 定义类和创建对象.py
 # Software: PyCharm
 
 """
@@ -81,14 +81,80 @@
 """
 第二次PK，面向对象做些许改版
 """
-class Bar:
-    def foo(self,hobby):
-        print("%s,%s岁,%s,%s" % (self.name, self.age, self.sex, hobby))
+
+# class Bar:
+#     def foo(self, hobby):
+#         print("%s,%s岁,%s,%s" % (self.name, self.age, self.sex, hobby))
+#
+#
+# obj1 = Bar()
+# obj1.name = '小明'
+# obj1.age = 18
+# obj1.sex = '男'
+#
+# obj1.foo(hobby='上山去打柴')
+#
+# obj2 = Bar()
+# obj2.name = '老李'
+# obj2.age = 90
+# obj2.sex = '男'
+#
+# obj2.foo(hobby='上山去打柴')
+
+"""
+构造方法:
+    特殊的作用：
+        在 obj = 类名()时做了两件事：
+            1、创建对象
+            2、通过对象执行类中的一个特殊方法__init__构造方法
+构造方法的特性：
+    类名()自动执行构造方法
+"""
 
 
-obj = Bar()
-obj.name = '小明'
-obj.age = 18
-obj.sex = '男'
+class Person:
+    def __init__(self, name, age):
+        """
+        构造方法
+        :param name:
+        :param age:
+        """
+        self.n = name
+        self.a = age
 
-obj.foo(hobby='上山去打柴')
+    def show(self):
+        print('%s-%s' % (self.n, self.a))
+
+
+baiy = Person('白洋', 19)
+leib = Person('雷冰', 18)
+baiy.show()
+leib.show()
+
+"""
+总结：
+1、如何创建类：
+    class 类名:
+        pass
+        
+2、创建方法：
+    1、构造方法：__init__(self.arg)
+        obj = 类() 创建对象的时候自动执行        
+    2、普通方法
+        obj = 类('xxx')
+        obj.普通方法名()
+        
+3、面向对象的三大特性之一:封装
+    class Bar:
+        def __init__(self, n, a)
+            self.name = n
+            self.age = a
+            self.blood = 'o'
+    b1 = Bar('alex', 123)
+    
+    b2 = Bar('eric', 456)
+    
+    
+4、适用场景：
+    如果多个函数中有一些相同参数时，可将函数式编程转换成面向对象
+"""
